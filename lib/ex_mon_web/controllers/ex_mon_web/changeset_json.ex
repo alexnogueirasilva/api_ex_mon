7b7770b4c15b5062ce1f86eb784c0f821f49288b
Alex Nogueira <alex@devaction.com.br>
2023-05-31 19:32:58 -0300

    feature(app): delete trainers

--- lib/ex_mon_web/controllers/ex_mon_web/changeset_json.ex
+++ lib/ex_mon_web/controllers/ex_mon_web/changeset_json.ex
@@ -2,8 +2,12 @@ defmodule ExMonWeb.ChangesetJSON do
   @moduledoc false
   import Ecto.Changeset, only: [traverse_errors: 2]
 
-  def render("bad.json", %{result: result}) do
+  def render("bad.json", %{result: %Ecto.Changeset{} = result}) do
     %{message: translate_erros(result)}
+  end
+
+  def render("bad.json", %{result: message}) do
+    %{message: message}
   end
 
   defp translate_erros(changeset) do
--- lib/ex_mon_web/controllers/ex_mon_web/trainers_controller.ex
+++ lib/ex_mon_web/controllers/ex_mon_web/trainers_controller.ex
@@ -11,11 +11,24 @@ defmodule ExMonWeb.TrainersController do
     |> handel_response(conn)
   end
 
+  def delete(conn, %{"id" => id}) do
+    id
+    |> ExMon.delete_trainer()
+    |> handel_delete(conn)
+  end
+
   defp handel_response({:ok, trainer}, conn) do
     conn
     |> put_status(:created)
     |> render(:create, trainer: trainer)
   end
 
+  defp handel_delete({:ok, _trainer}, conn) do
+    conn
+    |> put_status(:no_content)
+    |> text("")
+  end
+
   defp handel_response({:error, _changeset} = error, _conn), do: error
+  defp handel_delete({:error, _reason} = error, _conn), do: error
 end
